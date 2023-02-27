7908ba17b30b00e61aacc5c313e835ab5a9d4157
GappleCider <117851605+GappleCider@users.noreply.github.com>
2023-02-27 12:30:11 -0800

    Add files via upload

---
+++ build/sundrop.cjs
@@ -0,0 +1,262 @@
+"use strict";
+
+const canvas = document.createElement( "canvas" );
+const ctx = canvas.getContext( "2d" );
+const _canvasWidth = canvas.width = window.innerWidth;
+const _canvasHeight = canvas.height = window.innerHeight;
+
+function setCanvasColor( color ) {
+
+  canvas.style.backgroundColor = color;
+
+}
+
+function setCanvasAlpha( alpha ) {
+
+  canvas.style.opacity = alpha;
+
+}
+
+document.body.appendChild( canvas );
+
+class Vec2 {
+
+  constructor( x, y ) {
+
+    this.pos = {
+      x: x,
+      y: y
+    }
+
+    return this.pos
+  }
+
+}
+
+class Polygon {
+
+  constructor( points = [], props ) {
+
+    this.props = {
+      visible: true
+    };
+
+    this.polygon = points;
+    this.tags = {};
+
+    this.class = 'polygon'
+
+    this.points.forEach( ( point ) => {
+        
+      point.push( new Vec2( point[0], point[1] ) )
+
+      point.splice(0, 2)
+
+    })
+
+  }
+
+}
+
+class Line {
+
+  constructor( startX, startY, endX, endY ) {
+
+    this.startPoint = new Vec2( startX, startY )
+    this.endPoint = new Vec2( endX, endY )
+
+    this.props = {
+      visible: true
+    }
+
+    this.tags = {
+
+    }
+
+    this.type = "line"
+  }
+
+}
+
+class Box extends Polygon {
+
+  constructor( x, y, width, height ) {
+
+    super ( [] )
+        
+    this.pos = {
+      x: x,
+      y: y
+    }
+    this.width = width
+    this.height = height
+
+    this.type = 'box'
+
+    this.polygon = this.createPolygon( )
+  }
+
+  createPolygon() {
+
+    let points = []
+
+    points.push({
+      x: this.pos.x - ( this.width / 2 ),
+      y: this.pos.y - ( this.height / 2 )
+    })
+
+    points.push({
+      x: this.pos.x + ( this.width / 2 ),
+      y: this.pos.y - ( this.height / 2 )
+    })
+
+    points.push({
+      x: this.pos.x + ( this.width / 2),
+      y: this.pos.y + ( this.height / 2)
+    })
+
+    points.push({
+      x: this.pos.x - ( this.width / 2 ),
+      y: this.pos.y + ( this.height / 2 )
+    })
+
+    return points
+  }
+}
+
+class Circle {
+
+  constructor( x, y, radius) {
+
+    this.pos = {
+      x: x,
+      y: y
+    }
+
+    this.radius = radius
+    this.type = 'circle'
+
+  }
+
+}
+
+class Texture {
+
+  constructor( url ) {
+
+    this.loaded = false
+    this.image = new Image()
+    this.image.src = url
+
+    this.image.onload = () => {
+
+      this.loaded = true
+
+    }
+
+  }
+
+}
+
+  /*class TexturedBox extends Box {
+
+    constructor( x, y, width, height, url ){
+
+      super( x, y, width, height ) 
+
+      this.texture = new Texture( url )
+
+      this.class = null
+      this.type = 'texturedBox'
+
+    }
+
+  }*/
+
+class Renderer {
+
+  constructor() {
+      
+    this.objs = []
+
+  }
+    
+  renderer() {
+      
+    ctx.clearRect( 0, 0, _canvasWidth, _canvasHeight )
+
+    for ( let i = 0; i < this.objs.length; i++ ) {
+
+      if ( this.objs[i].props.drawStyle === 'fill' ) {
+        this.objs[i].props.color != undefined
+        ? ctx.fillStyle = this.objs[i].props.color
+        : ctx.fillStyle = 'black'
+      }
+      else if ( this.objs[i].props.drawStyle === 'stroke' ) {
+        this.objs[i].props.color != undefined
+        ? ctx.strokeStyle = this.objs[i].props.color + ' ' + this.objs[i].props.strokeWidth + 'px'
+        : ctx.strokeStyle = 'black'
+      }
+
+      if ( this.objs[i].class === 'polygon' ) {
+
+        ctx.beginPath()
+        ctx.moveTo( this.objs[i].polygon[0].x, this.objs[i].polygon[0].y )
+        for ( let j = 1; j < this.objs[i].polygon.length; j++ ) {
+
+          ctx.lineTo( this.objs[i].polygon[j].x, this.objs[i].polygon[j].y )
+
+        }
+        ctx.lineTo( this.objs[i].polygon[0].x, this.objs[i].polygon[0].y )
+        ctx.closePath()
+
+        if ( this.objs[i].props.drawStyle === 'fill' ) {
+
+          ctx.fill()
+
+        }
+        else if ( this.objs[i].props.drawStyle === 'stroke' ) {
+
+          ctx.stroke()
+
+        }
+
+      }
+
+    }
+
+  } 
+  add( obj ) {
+
+    this.objs.push( obj ) 
+
+  }
+  remove( obj ) {
+
+    this.objs.splice( this.objs.indexOf( obj ), 1 )
+
+  }
+  clear() {
+
+    this.objs = []
+
+  }
+  get( obj ) {
+
+    return this.objs.indexOf( obj )
+
+  }
+
+}
+
+exports.canvas = canvas;
+exports.ctx = ctx;
+exports.setCanvasColor = setCanvasColor
+exports.setCanvasAlphs = setCanvasAlpha
+exports.Vec2 = Vec2
+exports.Polygon = Polygon
+exports.Line = Line
+exports.Box = Box
+exports.Circle = Circle
+exports.Texture = Texture
+exports.Renderer = Renderer
---
+++ build/sundrop.module.js
@@ -0,0 +1,455 @@
+const canvas = document.createElement( "canvas" );
+const ctx = canvas.getContext( "2d" );
+const _canvasWidth = canvas.width = window.innerWidth;
+const _canvasHeight = canvas.height = window.innerHeight;
+
+function setCanvasColor( color ) {
+
+  canvas.style.backgroundColor = color;
+
+}
+
+function setCanvasAlpha( alpha ) {
+
+  canvas.style.opacity = alpha;
+
+}
+
+document.body.appendChild( canvas );
+
+class Vec2 {
+
+  constructor( x, y ) {
+
+    this.pos = {
+      x: x,
+      y: y
+    }
+
+    return this.pos
+  }
+
+}
+
+class Polygon {
+
+  constructor( points = [] ) {
+
+    this.props = {
+      visible: true
+    };
+
+    this.polygon = points;
+    this.tags = {};
+
+    this.class = 'polygon'
+
+    this.points.forEach( ( point ) => {
+        
+      this.polygon.push( new Vec2( point[0], point[1] ) )
+
+      point.splice(0, 2)
+
+    })
+
+  }
+
+}
+
+class Line {
+
+  constructor( startX, startY, endX, endY ) {
+
+    this.startPoint = new Vec2( startX, startY )
+    this.endPoint = new Vec2( endX, endY )
+
+    this.props = {
+      visible: true
+    }
+
+    this.tags = {
+
+    }
+
+    this.type = "line"
+  }
+
+}
+
+class Box {
+
+  constructor( x, y, width, height ) {
+        
+    this.pos = {
+      x: x,
+      y: y
+    }
+    this.width = width
+    this.height = height
+
+    this.class = 'polygon'
+    this.type = 'box'
+
+    this.polygon = this.createPolygon( )
+  }
+
+  createPolygon() {
+
+    let points = []
+
+    points.push({
+      x: this.pos.x - ( this.width / 2 ),
+      y: this.pos.y - ( this.height / 2 )
+    })
+
+    points.push({
+      x: this.pos.x + ( this.width / 2 ),
+      y: this.pos.y - ( this.height / 2 )
+    })
+
+    points.push({
+      x: this.pos.x + ( this.width / 2),
+      y: this.pos.y + ( this.height / 2)
+    })
+
+    points.push({
+      x: this.pos.x - ( this.width / 2 ),
+      y: this.pos.y + ( this.height / 2 )
+    })
+
+    return points
+  }
+
+  update() {
+
+    this.polygon = this.createPolygon( )
+
+  }
+}
+
+class Circle {
+
+  constructor( x, y, radius ) {
+
+    this.pos = {
+      x: x,
+      y: y
+    }
+
+    this.radius = radius
+    this.type = 'circle'
+
+  }
+
+}
+
+class Texture {
+
+  constructor( url ) {
+
+    this.loaded = false
+    this.image = new Image()
+    this.image.src = url
+
+    this.image.onload = () => {
+
+      this.loaded = true
+
+    }
+
+  }
+
+}
+
+  /*class TexturedBox extends Box {
+
+    constructor( x, y, width, height, url ){
+
+      super( x, y, width, height ) 
+
+      this.texture = new Texture( url )
+
+      this.class = null
+      this.type = 'texturedBox'
+
+    }
+
+  }*/
+
+class Renderer {
+
+  constructor() {
+      
+    this.objs = []
+    
+    ctx.fillStyle = 'black'
+
+    //this.render()
+
+  }
+
+
+
+
+  render() {
+
+    ctx.clearRect(0, 0, _canvasWidth, _canvasHeight)
+
+    for ( let i = 0; i < this.objs.length; i++ ) {
+
+      this.objs[i].props.color != undefined
+      ? ctx.fillStyle = this.objs[i].props.color
+      : ctx.fillStyle = 'black'
+
+      if ( this.objs[i].class === 'polygon' ) {
+
+        if ( this.objs[i].props.visible ) {
+          ctx.beginPath()
+          ctx.moveTo( this.objs[i].polygon[0].x, this.objs[i].polygon[0].y )
+          for ( let j = 0; j < this.objs[i].polygon.length - 1; j++ ) {
+
+            ctx.lineTo( this.objs[i].polygon[j + 1].x, this.objs[i].polygon[j + 1].y )
+
+          }
+          ctx.lineTo( this.objs[i].polygon[0].x, this.objs[i].polygon[0].y )
+          ctx.closePath()
+
+          if ( this.objs[i].props.drawStyle === 'fill' ) {
+
+            ctx.fill()
+
+          }
+          else if ( this.objs[i].props.drawStyle === 'stroke' ) {
+
+            ctx.stroke()
+
+          }
+
+        }
+
+      }
+
+      if ( this.objs[i].type === 'line' ) {
+
+        if ( this.objs[i].props.visible ) {
+
+          ctx.beginPath()
+          ctx.moveTo( this.objs[i].startPoint.x, this.objs[i].startPoint.y )
+          ctx.lineTo( this.objs[i].endPoint.x, this.objs[i].endPoint.y )
+          ctx.closePath()
+          ctx.stroke()
+
+        }
+
+      }
+      
+      if ( this.objs[i].type === 'circle' ) {
+        
+        if ( this.objs[i].props.visible ) {
+          
+          ctx.beginPath()
+          ctx.arc( 
+            this.objs[i].pos.x, 
+            this.objs[i].pos.y, 
+            this.objs[i].radius, 
+            0, 
+            Math.PI * 2
+          )
+          ctx.closePath()
+          if ( this.objs[i].props.drawStyle === 'fill' ) {
+
+            ctx.fill()
+
+          }
+          if ( this.objs[i].props.drawStyle === 'stroke' ) {
+
+            ctx.stroke()
+
+          }
+
+        }
+
+      }
+
+    }
+
+  } 
+  add( obj ) {
+
+    this.objs.push( obj ) 
+
+  }
+  remove( obj ) {
+
+    this.objs.splice( this.objs.indexOf( obj ), 1 )
+
+  }
+  clear() {
+
+    this.objs = []
+
+  }
+  get( obj ) {
+
+    return this.objs.indexOf( obj )
+
+  }
+  update() {
+
+    for ( let i = 0; i < this.objs.length; i++ ) {
+
+      if ( this.objs[i].type === 'box' ) {
+
+        this.objs[i].update( )
+
+      }
+
+    }
+
+  }
+
+}
+
+function lerp( a, b, t ) {
+
+  return a + ( b - a ) * t;
+
+}
+
+function randomInt( min, max ) {
+
+  return Math.floor( Math.random() * ( max - min ) ) + min;
+
+}
+
+function randomFloat( min, max ) {
+
+  return Math.random() * ( max - min ) + min;
+
+}
+
+function checkCollision(shape1, shape2) {
+
+  if (shape1.type === "circle" && shape2.type === "circle") {
+
+    const a = shape1.radius + shape2.radius;
+    const x = shape1.pos.x - shape2.pos.x;
+    const y = shape1.pos.y - shape2.pos.y;
+
+    if (a > Math.sqrt(x * x + y * y)) {
+
+      return true;
+
+    } else {
+
+      return false;
+
+    }
+
+  }
+
+  if (shape1.class === "polygon" && shape2.class === "polygon") {
+
+    function getIntersection(A, B, C, D) {
+
+      const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+      const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+      const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+
+      if (bottom != 0) {
+
+        const t = tTop / bottom;
+        const u = uTop / bottom;
+        if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+          return {
+
+            x: lerp(A.x, B.x, t),
+            y: lerp(A.y, B.y, t),
+            offset: t,
+
+          };
+
+        }
+
+      }
+
+      return null;
+
+    }
+
+    for (let i = 0; i < shape1.polyon; i++) {
+
+      for (let j = 0; j < shape2.polyon; j++) {
+
+        const intersection = getIntersection(
+          shape1.points[i],
+          shape2.points[j],
+          shape1.points[(i + 1) % shape1.polyon],
+          shape2.points[(j + 1) % shape2.polyon]
+        );
+
+        if (intersection) {
+
+          return true;
+
+        }
+
+      }
+
+    }
+
+    return false;
+
+  }
+
+  if (shape1.class === "polygon" && shape2.type === "circle") {
+
+    const distX = Math.abs(shape2.pos.x - shape1.pos.x);
+    const distY = Math.abs(shape2.pos.y - shape1.pos.y);
+
+    if (
+
+      distX > shape1.width / 2 + shape2.radius ||
+      distY > shape1.height / 2 + shape2.radius
+
+    ) {
+
+      return false;
+
+    }
+
+    if (distX <= shape1.width / 2 || distY <= shape1.height / 2) {
+
+      return true;
+
+    }
+
+  }
+
+  if (shape1.type === "circle" && shape2.class === "polygon") {
+
+    const distX = Math.abs(shape1.pos.x - shape2.pos.x);
+    const distY = Math.abs(shape1.pos.y - shape2.pos.y);
+
+    if (
+
+      distX > shape2.width / 2 + shape1.radius ||
+      distY > shape2.height / 2 + shape1.radius
+
+    ) {
+
+      return false;
+
+    }
+
+    if (distX <= shape2.width / 2 || distY <= shape2.height / 2) {
+
+      return true;
+
+    }
+
+  }
+
+}
+
+export {canvas, ctx, _canvasWidth, _canvasHeight, setCanvasColor, setCanvasAlpha, Vec2, Polygon, Line, Box, Circle, Texture, Renderer, lerp, randomInt, randomFloat, checkCollision}
