e80f52f17943932a55f2340cef858b51d72fffaf
GappleCider <117851605+GappleCider@users.noreply.github.com>
2023-02-28 09:53:01 -0800

    Update sundrop.cjs

--- build/sundrop.cjs
+++ build/sundrop.cjs
@@ -343,6 +343,147 @@ class Renderer {
 
 
 }
+function lerp( a, b, t ) {
+
+  return a + ( b - a ) * t;
+
+}
+
+function randomInt( min, max ) {
+
+  return Math.floor( Math.random() * ( max - min ) ) + min;
+
+}
+
+function randomFloat( min, max ) {
+
+  return Math.random() * ( max - min ) + min;
+
+}
+
+function checkCollision(shape1, shape2) {
+
+  if (shape1.type === "circle" && shape2.type === "circle") {
+
+    const a = shape1.radius + shape2.radius;
+    const x = shape1.pos.x - shape2.pos.x;
+    const y = shape1.pos.y - shape2.pos.y;
+
+    if (a > Math.sqrt(x * x + y * y)) {
+
+      return true;
+
+    } else {
+
+      return false;
+
+    }
+
+  }
+
+  if (shape1.class === "polygon" && shape2.class === "polygon") {
+
+    function getIntersection(A, B, C, D) {
+
+      const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+      const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+      const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+
+      if (bottom != 0) {
+
+        const t = tTop / bottom;
+        const u = uTop / bottom;
+        if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+          return {
+
+            x: lerp(A.x, B.x, t),
+            y: lerp(A.y, B.y, t),
+            offset: t,
+
+          };
+
+        }
+
+      }
+
+      return null;
+
+    }
+
+    for (let i = 0; i < shape1.polyon; i++) {
+
+      for (let j = 0; j < shape2.polyon; j++) {
+
+        const intersection = getIntersection(
+          shape1.points[i],
+          shape2.points[j],
+          shape1.points[(i + 1) % shape1.polyon],
+          shape2.points[(j + 1) % shape2.polyon]
+        );
+
+        if (intersection) {
+
+          return true;
+
+        }
+
+      }
+
+    }
+
+    return false;
+
+  }
+
+  if (shape1.class === "polygon" && shape2.type === "circle") {
+
+    const distX = Math.abs(shape2.pos.x - shape1.pos.x);
+    const distY = Math.abs(shape2.pos.y - shape1.pos.y);
+
+    if (
+
+      distX > shape1.width / 2 + shape2.radius ||
+      distY > shape1.height / 2 + shape2.radius
+
+    ) {
+
+      return false;
+
+    }
+
+    if (distX <= shape1.width / 2 || distY <= shape1.height / 2) {
+
+      return true;
+
+    }
+
+  }
+
+  if (shape1.type === "circle" && shape2.class === "polygon") {
+
+    const distX = Math.abs(shape1.pos.x - shape2.pos.x);
+    const distY = Math.abs(shape1.pos.y - shape2.pos.y);
+
+    if (
+
+      distX > shape2.width / 2 + shape1.radius ||
+      distY > shape2.height / 2 + shape1.radius
+
+    ) {
+
+      return false;
+
+    }
+
+    if (distX <= shape2.width / 2 || distY <= shape2.height / 2) {
+
+      return true;
+
+    }
+
+  }
+
+}
 
 exports.Vec2 = Vec2
 exports.Polygon = Polygon
@@ -351,3 +492,7 @@ exports.Box = Box
 exports.Circle = Circle
 exports.Texture = Texture
 exports.Renderer = Renderer
+exports.lerp = lerp
+exports.randomInt = randomInt
+exports.randomFloat = randomFloat
+exports.checkCollision = checkCollision
