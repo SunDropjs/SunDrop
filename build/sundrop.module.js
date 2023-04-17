54864926b3130cab83e3fc7bb12f16b3e83a5888
GappleCider <117851605+GappleCider@users.noreply.github.com>
2023-04-17 11:00:13 -0700

    Update sundrop.module.js

--- build/sundrop.module.js
+++ build/sundrop.module.js
@@ -255,7 +255,7 @@ class BasicRenderer2D {
     this.objs = [];
     this.ctx.clearRect(0, 0, this._canvasWidth, this._canvasHeight);
   }
-  get(obj = Object) {
+  get Object(obj = Object) {
     return this.objs.indexOf(obj);
   }
   update() {
